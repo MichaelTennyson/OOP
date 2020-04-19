a92202bc1281cb17c80cde1746ea6222bf892057
MichaelTennyson <55544189+MichaelTennyson@users.noreply.github.com>
2020-04-19 20:16:29 +0100

    Add files via upload

--- serchEngine.java
+++ serchEngine.java
@@ -8,12 +8,21 @@
 package assignment1;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Dictionary;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.io.*;
+
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -21,34 +30,33 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
 
 public class serchEngine extends JFrame implements ActionListener{
 	JLabel label1, label2;
-	JButton button1, button2;
+	JButton button1, button2, button3;
 	JPanel northPanel, centrePanel, southPanel, westPanel, eastPanel;
 	TextField textBox;
-	JFileChooser f1, f2, f3;
-	File word1 = new File("D:\\Eclipse\\OOPsem2\\src\\word1.txt");
-	File word2 = new File("D:\\Eclipse\\OOPsem2\\src\\word2.txt");
-	File word3 = new File("D:\\Eclipse\\OOPsem2\\src\\word3.txt");
 
 	public serchEngine(String myTitle) {
 		super(myTitle);
-		setSize(300,300);
+		setSize(400,400);
 		
-		//initialising the labels, buttons and text box
-		label1 = new JLabel(" The search engine ");
-		label2 = new JLabel(" find what file has your word ");
-		button1 = new JButton("Open files");
-		button2 = new JButton("Enter");
-		textBox = new TextField("enter your word / sentence", 20);
-		f1 = new JFileChooser( word1);
-		f2 = new JFileChooser(word2);
-		f3 = new JFileChooser( word3);
+		//initializing the labels, buttons and text box
+		label1 = new JLabel(" The SEARCH");
+		label2 = new JLabel(" find what file has your word or sentence! ");
+		button1 = new JButton("check file 1");
+		button2 = new JButton("check file 2");
+		button3 = new JButton("check file 3");
+		textBox = new TextField("enter your word / sentence", 55);
 		
 		
-		//attach the buttons to the action listener
+		//attach the button and textField to the action listener
 		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
 		textBox.addActionListener(this);
 		
 		// setting up my border layout
@@ -65,8 +73,10 @@ public class serchEngine extends JFrame implements ActionListener{
 		//positioning panels
 		westPanel.add(label1);
 	    eastPanel.add(label2);
-	    centrePanel.add(button1);
-	    southPanel.add(textBox);
+	    southPanel.add(button1);
+	    southPanel.add(button2);
+	    southPanel.add(button3);
+	    northPanel.add(textBox);
 	    
 		
 		//adding the panels
@@ -78,27 +88,120 @@ public class serchEngine extends JFrame implements ActionListener{
 		setVisible(true);
 
 	}
+	
 
+	//method that checks if the word entered is found in file 1
+	public void checkfile1(String txt) throws IOException{
+		//create paths
+		Path p1 = Paths.get("src/word1.txt").toAbsolutePath();
+		
+		System.out.println(p1.toAbsolutePath());
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == button1) {
-			//files are opened
-			File file1 = f1.getSelectedFile();
-			File file2 = f2.getSelectedFile();
-			File file3 = f3.getSelectedFile();
-			JOptionPane.showMessageDialog(this, "files opened");
+		//create lists holding the contents of each file in the paths
+		List<String> f1 = Files.lines(p1).collect(Collectors.toList());
+		
+		//the following boolean variable essentially collects where
+		boolean inFile1 = f1.stream().anyMatch(p->p.equalsIgnoreCase(txt));
+		
+		if(inFile1 == true) {
+			JOptionPane.showMessageDialog(this, "The word/sentence: " + txt + " is found in file 1");
+			
 		}
+		else {
+			JOptionPane.showMessageDialog(this, "text not found");
+			
+		}
+		
+			
+	}
+
+	public void checkfile2(String txt) throws IOException{
+		//create path
+		Path p2 = Paths.get("src/word2.txt").toAbsolutePath();
+		
+		System.out.println(p2.toAbsolutePath());
+
+		//create lists holding the contents of each file in the paths
+		List<String> f2 = Files.lines(p2).collect(Collectors.toList());
+		
+		//the following boolean variable essentially collects where
+		boolean inFile2 = f2.stream().anyMatch(p->p.equalsIgnoreCase(txt));
+		
+		if(inFile2 == true) {
+			JOptionPane.showMessageDialog(this, "The word/sentence: " + txt + " is found in file 2");
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(this, "text not found");
+			
+		}
+	
+			
+	}
+
+	public void checkfile3(String txt) throws IOException{
+		//create path
+		Path p3 = Paths.get("src/word3.txt").toAbsolutePath();
+		
+		System.out.println(p3.toAbsolutePath());
+
+		//create lists holding the contents of each file in the paths
+		List<String> f3 = Files.lines(p3).collect(Collectors.toList());
+		
+		//the following boolean variable essentially collects where
+		boolean inFile3 = f3.stream().anyMatch(p->p.equalsIgnoreCase(txt));
+		
+		if(inFile3 == true) {
+			JOptionPane.showMessageDialog(this, "The word/sentence: " + txt + " is found in file 3");
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(this, "text not found");
+			
+		}
+			
+			
+	}
+
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e){
+		if(e.getSource() == button1) {
+			String text = textBox.getText();
+			JOptionPane.showMessageDialog(this, "Checking file 1");
+			try {
+				checkfile1(text);
+			} 
+			catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+				
+			}
+		
 		if(e.getSource() == button2) {
 			String text = textBox.getText();
-			JOptionPane.showMessageDialog(this, "Checking now");
-			if(text ==  f1.open()){
-				
-				
+			JOptionPane.showMessageDialog(this, "Checking file 2");
+			try {
+				checkfile2(text);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		if(e.getSource() == button3) {
+			String text = textBox.getText();
+			JOptionPane.showMessageDialog(this, "Checking file 3");
+			try {
+				checkfile3(text);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		}
 		
-		
 	}
-
+	
 }
